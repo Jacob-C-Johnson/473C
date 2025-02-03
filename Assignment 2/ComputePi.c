2d2049a6c00bc2ca575be0ba3d461942516a1b50
Jacob-C-Johnson <jacobj1789@gmail.com>
2025-02-03 13:30:35 -0500

    Init commit

---
+++ Assignment 2/ComputePi.c
@@ -0,0 +1,22 @@
+/*
+ * ComputePi.c - Main file for assignment 2 to compute pi
+ *
+ * Author: Jacob Johnson
+ * Date: 02/03/2025
+ *
+ * Assignment: Assignment 2
+ * Course: CSCI 473
+ * Version 1.0
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include "utilities.h"
+
+
+int main(int argc, char *argv[]) {
+    // take n from command line input
+    int n = atoi(argv[1]);
+    double pi = compute_pi(n);
+    printf("Estimate of pi is %f\n", pi);
+    return 0;
+}
